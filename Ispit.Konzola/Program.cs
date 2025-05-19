0af59d39425a2ca8b1c95d460a67d5eb1ccbb4c9
V15 G3\Dino <dinofranceschi1@gmail.com>
2025-05-19 19:40:51 +0200

    inicijalni psuh

---
+++ Ispit.Konzola/Program.cs
@@ -0,0 +1,47 @@
+﻿using Ispit.Model;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading;
+
+namespace Ispit.Konzola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("hr-HR");
+            var ucenici = new List<Ucenik>();
+
+            for (int i = 1; i < 4; i++)
+            {
+                Ucenik ucenik = new Ucenik();
+
+                Console.Clear();
+
+                Console.WriteLine("Unesite ime, prezime, datum rođenja i prosjek za tri učenika. \n");
+
+                Console.WriteLine($"Unesite ime za {i}. učenika");
+                ucenik.Ime = Console.ReadLine();
+
+                Console.WriteLine($"Unesite prezime za {i}. učenika");
+                ucenik.Prezime = Console.ReadLine();
+
+                Console.WriteLine($"Unesite datum rođenja za {i}. učenika");
+                ucenik.DatumRodenja = Convert.ToDateTime(Console.ReadLine());
+
+                Console.WriteLine($"Unesite prosjek za {i}. učenika");
+                ucenik.Prosjek = double.Parse(Console.ReadLine());
+
+                ucenici.Add(ucenik);
+            }
+
+            foreach (var ucenik in ucenici)
+            {
+                Console.WriteLine($"\n Učenik {ucenik.Ime + " " + ucenik.Prezime}, dob {ucenik.Starost(ucenik.DatumRodenja)} godine, ima prosjek: {ucenik.IspisProsjeka(ucenik.Prosjek)}");
+            }
+
+
+        }
+    }
+}
---
+++ Ispit.Model/Ucenik.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Ispit.Model
+{
+    public class Ucenik
+    {
+        #region Fields
+        private string ime;
+        private string prezime;
+        private DateTime datumRodenja;
+        private double prosjek;
+        #endregion
+
+        #region Properties
+        public string Ime
+        {
+            get { return ime; }
+            set { ime = value; }
+        }
+
+        public string Prezime
+        {
+            get { return prezime; }
+            set { prezime = value; }
+        }
+
+        public DateTime DatumRodenja
+        {
+            get { return datumRodenja; }
+            set { datumRodenja = value; }
+        }
+
+        public double Prosjek
+        {
+            get { return prosjek; }
+            set { prosjek = value; }
+        }
+        #endregion
+
+        #region Methods
+        public int? Starost(DateTime datumRodenja)
+        {
+            if (datumRodenja > DateTime.Now)
+            {
+                return null;
+            }
+
+            DateTime tekuciDatum = DateTime.Now;
+            int starost = tekuciDatum.Year - datumRodenja.Year;
+
+            return starost;
+        }
+        public string IspisProsjeka(double prosjek)
+        {
+            if (prosjek >= 1 && prosjek <= 1.49)
+                return "Nedovoljan";
+            else if (prosjek >= 1.50 && prosjek <= 2.49)
+                return "Dovoljan";
+            else if (prosjek >= 2.50 && prosjek <= 3.49)
+                return "Dobar";
+            else if (prosjek >= 3.50 && prosjek <= 4.49)
+                return "Vrlo dobar";
+            else if (prosjek >= 4.50 && prosjek <= 5)
+                return "Odličan";
+
+            return "Nevažeći prosjek";
+        }
+        #endregion
+    }
+}
